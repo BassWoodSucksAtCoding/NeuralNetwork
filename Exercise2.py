4d0ddb54ebf710c99975fad504f837fb3d5c51e6
BassWoodSucksAtCoding <thebasswoodwhocriedpancake@gmail.com>
2026-07-10 22:21:15 +0600

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,36 @@
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device")
+
+## Easy
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28,512),
+            nn.ReLU(),
+            nn.Linear(512,512),
+            nn.Sigmoid(),
+            nn.Linear(512, 10)
+        )
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+# Output shape = (1,10)
+
+
+## Medium
+# 20 inputs
+# 10 outputs
+# 5 inputs are converted into 20 new outputs
+# XXX 5 images are present and the 20 features are converted into 10 images
+
